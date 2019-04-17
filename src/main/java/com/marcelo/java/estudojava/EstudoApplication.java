15b8d2043b7231d7ac7acdbd5c594ec3a7558ae2
Marcelo Luiz Jurevicius do Nascimento <marcelo.nascimento@livelo.com.br>
2019-04-17 09:41:47 -0300

    iniciando crypto

--- src/main/java/com/marcelo/java/estudojava/EstudoApplication.java
+++ src/main/java/com/marcelo/java/estudojava/EstudoApplication.java
@@ -1,13 +1,29 @@
 package com.marcelo.java.estudojava;
 
+import com.marcelo.java.estudojava.utils.SenhaUtils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EstudoApplication {
+
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			String senhaEncoded = SenhaUtils.gerarBCrypt("teste");
+			System.out.println("### Senha Encoded = " + senhaEncoded);
+			System.out.println("### Quantidade de elementos por página = " + this.qtdPorPagina);
+		};
+	}
 }
--- src/test/java/com/marcelo/java/estudojava/EstudoApplicationTests.java
+++ src/test/java/com/marcelo/java/estudojava/EstudoApplicationTests.java
@@ -1,16 +1,28 @@
 package com.marcelo.java.estudojava;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class EstudoApplicationTests {
+
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdPorPagina;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testCarregarContextoTeste() {
+		assertEquals(100, qtdPorPagina);
+	}
 }
